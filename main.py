5ca2dba19f0f4f07119d71ee25c1fabee7756c0f
elabs <elijahknsubuga@gmail.com>
2021-10-19 08:45:30 +0300

    mvp

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from phonenumbers import carrier, geocoder, timezone
 
 mob = input("enter number: ")
 mob = phonenumbers.parse(mob)
+
+print(timezone.time_zones_for_number(mob))
+print(carrier.name_for_number(mob, "en"))
+print(geocoder.description_for_number(mob, "en"))
+print("Valid Mobile Number: ", phonenumbers.is_valid_number(mob))
+print("Checking possbility of number: ", phonenumbers.is_possible_number(mob))
